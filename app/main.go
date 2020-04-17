da8255556e9434cd219c083896ce8a34936c9d49
Michael Shebeko <michael.shebeko@hcl.com>
2020-04-17 20:54:40 +0300

    Second stage of challenge

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+func main() {
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+}
